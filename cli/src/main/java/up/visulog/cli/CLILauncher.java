0eeeb7edb9f8c856bbb20d31bd314d4a844bb758
Lyrdinn <marinasecord2@gmail.com>
2021-09-26 20:19:04 +0200

    Added comments to CLILauncher.java

--- cli/src/main/java/up/visulog/cli/CLILauncher.java
+++ cli/src/main/java/up/visulog/cli/CLILauncher.java
@@ -11,18 +11,31 @@ import java.util.Optional;
 public class CLILauncher {
 
     public static void main(String[] args) {
+        //Creates a new configuration with the gitpath and the plugins as arguments from main.
         var config = makeConfigFromCommandLineArgs(args);
+
+        //If the configuration was correctly created :
         if (config.isPresent()) {
+            //We create a new analyzer object using this configuration as an argument.
             var analyzer = new Analyzer(config.get());
+            //The analyzer then runs all the plugins.
             var results = analyzer.computeResults();
+
             System.out.println(results.toHTML());
-        } else displayHelpAndExit();
+        } 
+        else displayHelpAndExit();
     }
 
+    //So what this code do is take the command line we entered from the terminal, and make a new configuration.
     static Optional<Configuration> makeConfigFromCommandLineArgs(String[] args) {
+        //gitPath takes the current path visulog is located at.
         var gitPath = FileSystems.getDefault().getPath(".");
         var plugins = new HashMap<String, PluginConfig>();
+
+        //This code looks at every argument ('''word''') of the string we entered in the terminal.
         for (var arg : args) {
+            //If the argument starts with "--" that means we will be passing program arguments. We are passing what we
+            //specifically want the program to do.
             if (arg.startsWith("--")) {
                 String[] parts = arg.split("=");
                 if (parts.length != 2) return Optional.empty();
@@ -30,6 +43,8 @@ public class CLILauncher {
                     String pName = parts[0];
                     String pValue = parts[1];
                     switch (pName) {
+                        //This is the argument that says we will add commit. This is VERY IMPORTANT, as when we will add new
+                        //plugins we will need to modify this code to check which plugin we are adding to our gradle.
                         case "--addPlugin":
                             // TODO: parse argument and make an instance of PluginConfig
 
@@ -39,6 +54,7 @@ public class CLILauncher {
                             });
 
                             break;
+                        
                         case "--loadConfigFile":
                             // TODO (load options from a file)
                             break;
@@ -49,7 +65,9 @@ public class CLILauncher {
                             return Optional.empty();
                     }
                 }
-            } else {
+            } 
+            
+            else {
                 gitPath = FileSystems.getDefault().getPath(arg);
             }
         }
