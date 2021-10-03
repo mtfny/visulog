d9f0299471406af3280002d3ef158eb41962d4f3
Marius Cahagne <marius.cahagne@gmail.com>
2021-10-03 16:08:03 +0200

    Changed the type of the date attribute and added a new method setDate.

--- gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
@@ -1,9 +1,14 @@
 package up.visulog.gitrawdata;
 
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class CommitBuilder {
     private final String id;
     private String author;
-    private String date;
+    private Date date;
     private String description;
     private String mergedFrom;
 
@@ -15,10 +20,21 @@ public class CommitBuilder {
         this.author = author;
         return this;
     }
-
-    public CommitBuilder setDate(String date) {
+    
+    public CommitBuilder setDate(String dateSt) { // Convert a String into a Date and change the date attribute of the commit
+	SimpleDateFormat s= new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.ENGLISH);
+	Date date=null;
+	try {
+		date=s.parse(dateSt);
+	    } catch (ParseException e) {
+	      e.printStackTrace();
+	    }
         this.date = date;
         return this;
+    }
+    public CommitBuilder setDate(Date date) {
+    	this.date=date;
+    	return this;
     }
 
     public CommitBuilder setDescription(String description) {
