47caae22cd4440cdf011e8f9d74e3e4ec7828620
LAGES Lucas <lucas.lages@laposte.net>
2021-10-08 20:18:27 +0200

    Rewrote save and load functions

--- config/src/main/java/up/visulog/config/Configuration.java
+++ config/src/main/java/up/visulog/config/Configuration.java
@@ -1,40 +1,47 @@
 package up.visulog.config;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Configuration {
+public class Configuration implements java.io.Serializable {
 
-    private final Path gitPath;
-    private final Path configFilePath; //Directory where the configuration file will be stored
+	//Paths are stored as a String so they can be serializable
+    private final String gitPath;
+    private final String configFilePath; //Directory where the configuration file will be stored
     private final HashMap<String, PluginConfig> plugins;
-    private final String configFileName = "config.txt";
+    private static final String configFileName = "config.txt";
 
-    public Configuration(Path gitPath, Path cfPath, HashMap<String, PluginConfig> plugins) {
+    public Configuration(String gitPath, String cfPath, HashMap<String, PluginConfig> plugins) {
         this.gitPath = gitPath;
         this.configFilePath = cfPath;
         this.plugins = deepCopy(plugins);
     }
 
+    
     public Path getGitPath() {
-        return gitPath;
+        return Paths.get(gitPath);
     }
 
     public Map<String, PluginConfig> getPluginConfigs() {
-        return plugins;
+        return deepCopy(plugins);
     }
     
     //Will save a file that contains all plugins variables that can be customized by the user
-    public void saveConfigFile() {
+    /*public void saveConfigFile() {
     	File configFile = getConfigFile();
     	
     	try {
@@ -56,10 +63,43 @@ public class Configuration {
 			e.printStackTrace();
 		}
     	
+    }*/
+    
+    public void saveConfigFile() {
+    	try {
+    		File configFile = getConfigFile();
+    		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(configFile));
+    		oos.writeObject(this);
+    		oos.close();
+    		System.out.println("Config file saved to " + configFilePath + configFileName);
+    	}
+    	
+    	catch(IOException e) {
+    		e.printStackTrace();
+    		System.out.println("ERROR : Configuration file couldn't be saved");
+    	}
+    	
+    }
+    
+    public static Configuration loadConfigFile(String configFilePath) {
+    	try {
+    		File configFile = new File(configFilePath + configFileName);
+    		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(configFile));
+    		Configuration res = (Configuration)ois.readObject();
+    		ois.close();
+    		System.out.println("Config file Loaded");
+    		return res;
+    	}
+    	
+    	catch(IOException|ClassNotFoundException e) {
+    		e.printStackTrace();
+    		System.out.println("ERROR : Configuration file couldn't be loaded");
+    		return null;
+    	}
     }
     
     //Returns configuration file if it exists, else it will create one
-    public File getConfigFile() {
+    private File getConfigFile() {
 		File configFile = new File(configFilePath + configFileName);
     	try {
     		configFile.createNewFile();
@@ -73,7 +113,7 @@ public class Configuration {
     }
     
     //Returns a deep copy of the plugins map
-    public HashMap<String, PluginConfig> deepCopy(HashMap<String, PluginConfig> m){
+    private HashMap<String, PluginConfig> deepCopy(HashMap<String, PluginConfig> m){
     	HashMap<String, PluginConfig> res = new HashMap<String, PluginConfig>();
     	
     	for(Map.Entry<String, PluginConfig> entry : m.entrySet()) {
