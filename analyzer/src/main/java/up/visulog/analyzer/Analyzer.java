bc476e27a99bff5d13aa3fa5fc0a4861ba67b9c3
Hayaert <veroniquehayaert9@gmail.com>
2021-10-15 00:18:14 +0200

    Rewrote the makePlugin method of the Analyzer class so that AnalyzerPlugin instances could be instantiated without hard coding

--- analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
+++ analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.lang.reflect.*;
 
 public class Analyzer {
     private final Configuration config;
@@ -33,12 +34,30 @@ public class Analyzer {
         return new AnalyzerResult(plugins.stream().map(AnalyzerPlugin::getResult).collect(Collectors.toList()));
     }
 
-    // TODO: find a way so that the list of plugins is not hardcoded in this factory
+    //In order for the list of plugins not to be hardcored in this factory with a switch statement, this method will use the String "pluginName" (which is passed as a parameter) in order to find the class name of the object we want to instantiate.
     private Optional<AnalyzerPlugin> makePlugin(String pluginName, PluginConfig pluginConfig) {
-        switch (pluginName) {
-            case "countCommits" : return Optional.of(new CountCommitsPerAuthorPlugin(config));
-            default : return Optional.empty();
-        }
+    	
+    	if(this.config.getPluginConfigs().containsKey(pluginName)) {
+    		// If the "plugins" Map attribute of the config attribute of the Analyzer class contains a key that is equivalent to the parameter pluginName, a new object of the Class.forName(pluginName) class with the object "this.config" passed as parameter to the class constructor is returned.
+    		
+    		try {
+    			@SuppressWarnings("unchecked")
+    			//The class object which corresponds to the class of the plugin we want to create is retrieved.
+    			Class<?> c = (Class<AnalyzerPlugin>)Class.forName(pluginName);
+    			//The constructor(s) for this class are retrieved in an array.
+    			Constructor<?>[] pluginConstructor = c.getConstructors();
+    			//A new instance of the type of plugin we want to create is returned.
+    			return Optional.of((AnalyzerPlugin)pluginConstructor[0].newInstance(this.config));
+    		}
+    		catch(Exception e) {
+    			//If the method runs into an exception at any point in the try statement, an empty Optional instance is returned instead.
+    			return Optional.empty();
+    		}
+    	}
+    	
+    	//If the "plugins" Map attribute of the "config" Configuration attribute of the Analyzer class does not contain a key that is equivalent to the String parameter pluginName, an empty Optional instance is returned instead.
+    	return Optional.empty();
+    	
     }
-
+    
 }
