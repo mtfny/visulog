c74b6b68ebc1295aaea3ac19fc058a124d99a5b0
Hayaert <veroniquehayaert9@gmail.com>
2021-12-18 19:27:07 +0100

    Refactoring : Created abstract class extended by the plugins that may take a starting and/or ending date of the analysis into account

--- analyzer/src/main/java/up/visulog/analyzer/CountLinesPerDayPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountLinesPerDayPlugin.java
@@ -1,18 +1,16 @@
 package up.visulog.analyzer;
 import java.time.LocalDate;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import up.visulog.analyzer.CountCommitsPerDayPlugin.Result;
 import up.visulog.config.Configuration;
 import up.visulog.gitrawdata.Commit;
 import up.visulog.gitrawdata.NumberOfLinesPerAuthor;
 
-public class CountLinesPerDayPlugin implements AnalyzerPlugin{
+public class CountLinesPerDayPlugin extends DateAnalyzerPlugin{
 	private final Configuration configuration;
     private Result result;
     
@@ -83,49 +81,6 @@ public class CountLinesPerDayPlugin implements AnalyzerPlugin{
 	    	if(lines.size() != 0)
 	    		result = processLog(lines);
 		}
-    }
-    
-    public List<String> dateAnalysis(List<String> command, Map<String, String> settings){
-    	command = addDateOption(command, settings, dateDebutOption, "--since=");
-    	command = addDateOption(command, settings, dateFinOption, "--until=");
-		
-		return command;
-    }
-    
-    public List<String> addDateOption(List<String> command, Map<String, String> settings, String parameterName, String gitOptionName){
-    		//On verifie qu'on a bien des clés parameterName
-    			if (settings.containsKey(parameterName)) {
-    				String debut = settings.get(parameterName);
-    				
-    				//On créé des tableux de notre string de date.
-    				String[] date = debut.split("/");
-
-    				//On verifie que l'on un tableau de dates de taille 3 chacuns pour le JJ/MM/AAAA.
-    				if (date.length == 3) {
-    					//On regarde si chaque partie de notre string est une date valide.
-    					try{
-    						int day = Integer.parseInt(date[0]);
-    						int mois = Integer.parseInt(date[1]);
-    						int annee = Integer.parseInt(date[2]);
-
-    						DateObj dateObj = new DateObj(day, mois, annee);
-
-    						//Regarde si les jours sont bien entre 1 et 31, les mois entre 1 et 12 etc. avec l'aide du constructeur de
-    						//la date
-    						if (dateObj.getDay() != 0 && dateObj.getIntMonth() != 0 && dateObj.getYear() != 0) {
-
-    							//Alors on passe nos parametres à la fonction. Gitlog prend le format AAAA/MM/JJ
-    							String formatGL1 = date[2] + "-" + date[1] + "-" + date[0];
-    							command.add(gitOptionName + formatGL1);
-    						}
-    					}
-    					catch (NumberFormatException ex){
-    						ex.printStackTrace();
-    					}
-    				}
-    			}
-    			
-    			return command;
     }
     
     @Override
---
+++ analyzer/src/main/java/up/visulog/analyzer/DateAnalyzerPlugin.java
@@ -0,0 +1,54 @@
+package up.visulog.analyzer;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class DateAnalyzerPlugin implements AnalyzerPlugin{
+
+	/*
+	 * Abstract class extended by plugins that may take starting and/or ending dates into account during the analysis, if at least one such date is specified by the user
+	 */
+	
+	public List<String> dateAnalysis(List<String> command, Map<String, String> settings){
+    	command = addDateOption(command, settings, dateDebutOption, "--since=");
+    	command = addDateOption(command, settings, dateFinOption, "--until=");
+		
+		return command;
+    }
+    
+    public List<String> addDateOption(List<String> command, Map<String, String> settings, String parameterName, String gitOptionName){
+    		//On verifie qu'on a bien des clés parameterName
+    			if (settings.containsKey(parameterName)) {
+    				String debut = settings.get(parameterName);
+    				
+    				//On créé des tableux de notre string de date.
+    				String[] date = debut.split("/");
+
+    				//On verifie que l'on un tableau de dates de taille 3 chacuns pour le JJ/MM/AAAA.
+    				if (date.length == 3) {
+    					//On regarde si chaque partie de notre string est une date valide.
+    					try{
+    						int day = Integer.parseInt(date[0]);
+    						int mois = Integer.parseInt(date[1]);
+    						int annee = Integer.parseInt(date[2]);
+
+    						DateObj dateObj = new DateObj(day, mois, annee);
+
+    						//Regarde si les jours sont bien entre 1 et 31, les mois entre 1 et 12 etc. avec l'aide du constructeur de
+    						//la date
+    						if (dateObj.getDay() != 0 && dateObj.getIntMonth() != 0 && dateObj.getYear() != 0) {
+
+    							//Alors on passe nos parametres à la fonction. Gitlog prend le format AAAA/MM/JJ
+    							String formatGL1 = date[2] + "-" + date[1] + "-" + date[0];
+    							command.add(gitOptionName + formatGL1);
+    						}
+    					}
+    					catch (NumberFormatException ex){
+    						ex.printStackTrace();
+    					}
+    				}
+    			}
+    			
+    			return command;
+    }
+}
